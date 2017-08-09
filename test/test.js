1bd159a41c6788cf4f2782bb93632844c9f0bba6
BirdieKim <yjcheshire@gmail.com>
2017-08-09 22:07:00 +1000

    Automatically require all the files with prefix "_test"

--- test/test.js
+++ test/test.js
@@ -1,8 +1,12 @@
 var qunit = require('qunitjs');
 require('qunitjs/qunit/qunit.css');
 
-require('./temp_test');
+_requireAll(require.context('.', true, /_test$/));
 
 qunit.load();
 qunit.config.testTimeout = 5000;
 qunit.start();
+
+function _requireAll(r) {
+  return r.keys().map(r);
+}
