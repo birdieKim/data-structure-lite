039d680539ccc9cc479f7e77272569d0ba35183d
BirdieKim <yjcheshire@gmail.com>
2017-08-10 20:31:41 +1000

    Add clear() & isEmpty() to Queue

--- src/queue.js
+++ src/queue.js
@@ -3,28 +3,81 @@ var Queue = function() {
   this.front = 0;  // the index of where the element will be deleted
 };
 
-/*
- * @function - enqueue
+/**
  *
- * @Param {Any} element - to push
- * @Return {Any} - The element being poped
+ * Insert an element to the end of the queue
+ *
+ * @param {Any} element
+ *   The element to be inserted
+ *
+ * @fires push
+ *
+ * @return
+ *   New length of the queue after the insertion
  */
 Queue.prototype.enqueue = function(element) {
   this.elements.push(element);
 };
 
+/**
+ *
+ * Remove an element at the front of the queue
+ *
+ * @fires shift
+ *
+ * @return
+ *   The element just removed from the queue
+ */
 Queue.prototype.dequeue = function() {
   return this.elements.shift();
 };
 
+/**
+ *
+ * Get an element at the front of the queue without dequeueing
+ *
+ * @return
+ *   The element at the front of the queue
+ */
 Queue.prototype.peek = function() {
   return this.elements[this.front];
 };
 
+/**
+ *
+ * Set the index of where an element will be inserted
+ *
+ * @return
+ *   The index next to the last index of the queue
+ */
 Queue.prototype.rear = function() {
-  var len = this.elements.length - 1;
-  
+  var len = this.elements.length;
   return len < 0 ? 0 : len;
+};
+
+/**
+ *
+ * Make the array empty
+ *
+ */
+Queue.prototype.clear = function() {
+  this.elements.length = 0;
+};
+
+/**
+ *
+ * Check if the queue is empty
+ *
+ * @return
+ *   Boolean for whether the queue is empty or not
+ */
+Queue.prototype.isEmpty = function() {
+  var len = this.elements.length;
+  if(len > 0){
+    return false;
+  } else {
+    return true;
+  }
 };
 
 module.exports = Queue;
