a517c229774e0fc6a68c6fa8bde509e554fc6cb5
BirdieKim <yjcheshire@gmail.com>
2017-09-22 12:04:16 +1000

    Add the way to export each module

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,118 @@ switch (process.env.npm_lifecycle_event) {
       ]
     };
     break;
+  case 'prod_queue':
+    module.exports = {
+      entry: [
+        './queue.js'
+      ],
+      output: {
+        path: __dirname + '/dist/',
+        filename: 'queue.min.js'
+      },
+      plugins: [
+        new UglifyJSPlugin()
+      ]
+    };
+    break;
+  case 'prod_priorityQueue':
+    module.exports = {
+      entry: [
+        './priorityQueue.js'
+      ],
+      output: {
+        path: __dirname + '/dist/',
+        filename: 'priorityQueue.min.js'
+      },
+      plugins: [
+        new UglifyJSPlugin()
+      ]
+    };
+    break;
+  case 'prod_stack':
+    module.exports = {
+      entry: [
+        './stack.js'
+      ],
+      output: {
+        path: __dirname + '/dist/',
+        filename: 'stack.min.js'
+      },
+      plugins: [
+        new UglifyJSPlugin()
+      ]
+    };
+    break;
+  case 'prod_linkedList':
+    module.exports = {
+      entry: [
+        './linkedList.js'
+      ],
+      output: {
+        path: __dirname + '/dist/',
+        filename: 'linkedList.min.js'
+      },
+      plugins: [
+        new UglifyJSPlugin()
+      ]
+    };
+    break;
+  case 'prod_doublyLinkedList':
+    module.exports = {
+      entry: [
+        './doublyLinkedList.js'
+      ],
+      output: {
+        path: __dirname + '/dist/',
+        filename: 'doublyLinkedList.min.js'
+      },
+      plugins: [
+        new UglifyJSPlugin()
+      ]
+    };
+    break;
+  case 'prod_tree':
+    module.exports = {
+      entry: [
+        './tree.js'
+      ],
+      output: {
+        path: __dirname + '/dist/',
+        filename: 'tree.min.js'
+      },
+      plugins: [
+        new UglifyJSPlugin()
+      ]
+    };
+    break;
+  case 'prod_binarySearchTree':
+    module.exports = {
+      entry: [
+        './binarySearchTree.js'
+      ],
+      output: {
+        path: __dirname + '/dist/',
+        filename: 'binarySearchTree.min.js'
+      },
+      plugins: [
+        new UglifyJSPlugin()
+      ]
+    };
+    break;
+  case 'prod_heap':
+    module.exports = {
+      entry: [
+        './heap.js'
+      ],
+      output: {
+        path: __dirname + '/dist/',
+        filename: 'heap.min.js'
+      },
+      plugins: [
+        new UglifyJSPlugin()
+      ]
+    };
+    break;
   case 'test':
     module.exports = {
       entry: [
